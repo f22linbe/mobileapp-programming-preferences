68b7ca5635da0f8d8c9fad4f641e3455947d5e68
f22linbe <f22linbe@student.his>
2023-05-25 20:00:57 +0200

    Added SecondActivity with layout & buttons that runs or stops SecondActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button buttonActivity = findViewById(R.id.activity_button);
+
+        buttonActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button backButton = findViewById(R.id.back_button);
+
+        String itemString = getIntent().getStringExtra("itemString");
+
+        backButton.setOnClickListener(v -> finish());
+    }
+}
